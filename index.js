8cec5ffa4a68be9b949b934502bca81b2dd9e5d3
SANTIAGO <sjkinast@uc.cl>
2021-04-26 21:51:54 -0400

    arreglando

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ mongoose.connect(process.env.DB_CONNECTION,
 );
 
 
-// PORT
+// PORT & HOST
 const port  = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const host = process.env.HOST || '0.0.0.0';
+
+app.listen(port,host, () => console.log(`Listening on port ${port}...`));
